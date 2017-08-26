9ce60554121e5177002cfcb988d0ff430d8ff8a2
haxmod </>
2017-08-26 20:01:56 +0200

    Remove source port restriction

--- filter.sh
+++ filter.sh
@@ -1,1 +1,1 @@
-iptables -A INPUT -p udp -j NFQUEUE --sport 1024:65535 --dport 1024:65535 --queue-num 0 --queue-bypass
+iptables -A INPUT -p udp -j NFQUEUE --dport 1024:65535 --queue-num 0 --queue-bypass
